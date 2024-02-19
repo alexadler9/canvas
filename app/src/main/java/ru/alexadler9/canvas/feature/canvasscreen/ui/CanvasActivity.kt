1ce13b8957adb521009d1af31465f581036af8d7
alexadler9 <alexadler@bk.ru>
2024-02-19 22:38:07 +0500

    Added appbar

--- app/src/main/java/ru/alexadler9/canvas/feature/canvasscreen/ui/CanvasActivity.kt
+++ app/src/main/java/ru/alexadler9/canvas/feature/canvasscreen/ui/CanvasActivity.kt
@@ -1,11 +1,17 @@
 package ru.alexadler9.canvas.feature.canvasscreen.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import ru.alexadler9.canvas.R
 import ru.alexadler9.canvas.databinding.ActivityCanvasBinding
 
 private const val TAG = "CANVAS_ACTIVITY"
@@ -22,9 +28,30 @@ class CanvasActivity : AppCompatActivity() {
         binding = ActivityCanvasBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
+        // Add menu items using the MenuProvider API.
+
+        val menuHost: MenuHost = this
+        menuHost.addMenuProvider(object : MenuProvider {
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection.
+                return true
+            }
+        })
+
+        // Configure viewModel.
+
         viewModel.viewState
             .onEach(::render)
             .launchIn(lifecycleScope)
+
+        // Configure listeners for tools.
 
         with(binding) {
             layoutStyle.root.setOnClickListener {
